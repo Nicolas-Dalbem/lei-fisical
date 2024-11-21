9f06a05375fd79996807d6c9fc92e278e4876566
Nicolas-Dalbem <88493158+Nicolas-Dalbem@users.noreply.github.com>
2024-11-21 08:56:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ document.getElementById('pauseButton').addEventListener('click', function() {
     // Quando o botão é clicado, pausamos a animação da caixa
     if (box.style.animationPlayState !== 'paused') {
         box.style.animationPlayState = 'paused';
-        document.getElementById('description').innerText = 'Movimento pausado! Isso é como a inércia resiste à mudança de movimento.';
+        document.getElementById('description').innerText = 'Movimento interrompido! Isso é como a inércia reage à mudança de movimento.';
     } else {
         box.style.animationPlayState = 'running';
-        document.getElementById('description').innerText = 'Clique no botão para "interromper" o movimento, simulando a resistência à mudança de movimento (inércia).';
+        document.getElementById('description').innerText = 'Clique no botão para simular uma força sendo aplicada sobre o corpo, alterando seu estado de movimento.';
     }
 });
